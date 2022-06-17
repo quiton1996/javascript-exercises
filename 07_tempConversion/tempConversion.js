2ce8cbb8ef505e180446549f15206602e89891de
quiton <quiton1996@gmail.com>
2022-06-17 14:51:23 -0500

    solve problem 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9;
+  celsius = +celsius.toFixed(1);
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fahrenheit = celsius * 9/5 + 32;
+  fahrenheit = +fahrenheit.toFixed(1);
+  return fahrenheit;
 };
 
 // Do not edit below this line
