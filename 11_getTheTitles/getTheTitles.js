2b8f1f58433c8eb0dd403782d2b5861affd77663
quiton <quiton1996@gmail.com>
2022-06-23 23:55:02 -0500

    Solve problem 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(struct) {
+  let array = [];
+  for (let i = 0; i < struct.length; i++) {
+    array.push(struct[i].title);
+  }
 
+  return array;
 };
 
 // Do not edit below this line
