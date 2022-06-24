7d9a5c932f2e4b3e0c58c2ee0865260309a8cf9c
quiton <quiton1996@gmail.com>
2022-06-24 00:30:02 -0500

    Solve problem 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  let oldestAge = 0;
+  let index;
 
+  array.forEach((person, i) => {
+    let age;
+    if (!("yearOfDeath" in person)) {
+      age = 2022 - parseInt(person.yearOfBirth);
+    }
+    else {
+      age = person.yearOfDeath - person.yearOfBirth;
+    }
+    
+    if (age > oldestAge) {
+      oldestAge = age;
+      index = i;
+    }
+  })
+  array = array[index]
+  return array;
 };
 
 // Do not edit below this line
