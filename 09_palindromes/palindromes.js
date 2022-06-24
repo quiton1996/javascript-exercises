f31c599f9b91196da8e1ee78641265a51396713e
quiton <quiton1996@gmail.com>
2022-06-23 23:18:24 -0500

    Solve problem 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.replace(/,| |\.|!/g, "").toLowerCase();
+  let reversed = '';
+  for (let i = string.length-1; i >= 0; i--) {
+    reversed += string[i];
+  }
+  console.log(string, reversed);
 
+  if (reversed === string) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 // Do not edit below this line
