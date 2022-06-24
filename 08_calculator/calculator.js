843fc08d79b783343fd16cda1a7d71da12cfcbe3
quiton <quiton1996@gmail.com>
2022-06-23 22:48:36 -0500

    Solve problem 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
 const add = function() {
-	
+  let total = 0;
+	for (let i = 0; i < arguments.length; i++) {
+    total += parseInt(arguments[i]);
+  }
+  return total;
 };
 
 const subtract = function() {
-	
+	const diff = arguments[0] - arguments[1];
+  return diff
 };
 
 const sum = function() {
-	
+	let result = 0;
+  for (let i = 0; i < arguments[0].length; i++) {
+    result += parseInt(arguments[0][i]);
+  }
+  return result;
 };
 
 const multiply = function() {
-
+  let product = 1;
+  for (let i = 0; i < arguments[0].length; i++) {
+    product *= arguments[0][i];
+  }
+  console.log(product);
+  return product;
 };
 
 const power = function() {
-	
+	return (arguments[0] ** arguments[1]);
 };
 
 const factorial = function() {
-	
+	let fact = 1;
+  for (let i = arguments[0]; i > 1; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
@@ -31,3 +49,5 @@ module.exports = {
   power,
   factorial
 };
+
+multiply([2,3]);
