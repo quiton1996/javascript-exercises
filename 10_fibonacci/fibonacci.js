2107f98fcdb3b0360882dbbbafe3b2abf5de429c
quiton <quiton1996@gmail.com>
2022-06-23 23:31:31 -0500

    Solve problem 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+  let fib = [1, 1];
+  if (member < 1) {
+    return "OOPS";
+  }
 
+  if (member >= 3) {
+    for (let i = 2; i <= member - 1; i++) {
+      fib[i] = fib[i-1] + fib[i-2];
+    }
+  }
+  return fib[member - 1];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+fibonacci(1);
